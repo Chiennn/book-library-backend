f2a9d7d8493a62b99a671c60a5000b9c5c19ac1f
do.quoc.binh <binh.do@logigear.com>
2025-01-10 15:22:46 +0700

    config db

--- models/db.js
+++ models/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Linh@1909',
-    database: 'library_db'
+  host: process.env.DB_HOST || '127.0.0.1', // Sử dụng IPv4 localhost
+  user: process.env.DB_USER || 'root',     // Thay bằng username đúng
+  password: process.env.DB_PASSWORD || 'Linh@1909', // Thay bằng mật khẩu đúng
+  database: process.env.DB_NAME || 'library', // Thay bằng tên database đúng
+  port: process.env.DB_PORT || 3306        // Port MySQL (thường là 3306)
 });
 
-module.exports = connection;
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err.message);
+    return;
+  }
+  console.log('Connected to database');
+});
